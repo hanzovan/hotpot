9dc388bf3fe0795328338421f0601a7bdf04f727
Archer Nguyen <thivan.nms@gmail.com>
2022-05-25 09:08:50 +0000

    working on login route

--- application.py
+++ application.py
@@ -19,3 +19,22 @@ db = SQL("sqlite:///hotpot.db")
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+# Login page
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # If user reached route via submitting form:
+    if request.method == "POST":
+        # Define variable:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password_confirm = request.form.get("password_confirm")
+
+        if not username:
+            return render_template("error.html", message="Missing username")
+        if not
+
+    # If user reached route via clicking link or being redirect
+    else:
+        return render_template("login.html")
